bc038418268b686eb12579b85f9aa63ff2dce490
aws-bootcamp-137 <abdiadem137@gmail.com>
2023-03-22 23:32:25 +0000

    remove email from activities

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -3,11 +3,10 @@ VALUES
   ('Andrew Brown', 'andrewbrown' , 'testemail@gmail.com', 'MOCK'),
   ('Andrew Bayko', 'bayko' , 'email2@gmail.com', 'MOCK');
 
-INSERT INTO public.activities (user_uuid,email, message, expires_at)
+INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = %(handle)s  LIMIT 1),
-    testemail2@gmail.com,
+    (SELECT uuid from public.users WHERE users.handle = handle LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
