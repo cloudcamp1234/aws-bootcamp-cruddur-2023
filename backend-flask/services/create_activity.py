ada6029b4056a4a323e66d4f33307c47c1adc1ff
aws-bootcamp-137 <abdiadem137@gmail.com>
2023-03-22 20:45:39 +0000

    separate create activity sql use db.template

--- backend-flask/services/create_activity.py
+++ backend-flask/services/create_activity.py
@@ -1,6 +1,6 @@
 import uuid
 from datetime import datetime, timedelta, timezone
-from lib.db import query_commit
+from lib.db import db
 class CreateActivity:
   def run(message, user_handle, ttl):
     model = {
@@ -57,26 +57,13 @@ class CreateActivity:
     return model
 
     def create_activity(handle,message,expires_at):
-      sql = f"""
-      INSERT INTO (
-        user_uuid,
-        message,
-        expires_at
+      sql = db.template("create_activity")
+      uuid = db.query_commit(sql,{
+        "handle" : handle,
+        "message" : message,
+        "expires_at" : expires_at}
       )
-      VALUES (
-        (SELECT uuid from public.users 
-        WHERE users.handle = %(handle)s 
-        LIMIT 1)
-        %(message)s, 
-        %(expires_at)s, 
-        ) 
-        RETURNING uuid;
-      """
-      uuid = db.query_commit_with_returning_id(sql,
-        handle = handle,
-        message = message,
-        expires_at = expires_at
-      )
+      return uuid
       #try:
         #conn = pool.connection()
         #cur = conn.cursor()
@@ -86,4 +73,4 @@ class CreateActivity:
         #print_sql_err(err)
         #conn.rollback()
 
-      query_commit(sql)
+      #query_commit(sql)
