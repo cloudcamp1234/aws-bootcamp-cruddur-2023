437aac79c220d1f86a1d31c7bd779b2250f25000
aws-bootcamp-137 <abdiadem137@gmail.com>
2023-03-22 23:25:48 +0000

    add emails to seed data

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,12 +1,13 @@
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown' , 'email@email', 'MOCK'),
-  ('Andrew Bayko', 'bayko' , 'email2@email', 'MOCK');
+  ('Andrew Brown', 'andrewbrown' , 'testemail@gmail.com', 'MOCK'),
+  ('Andrew Bayko', 'bayko' , 'email2@gmail.com', 'MOCK');
 
-INSERT INTO public.activities (user_uuid, message, expires_at)
+INSERT INTO public.activities (user_uuid,email, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = %(handle)s  LIMIT 1),
+    testemail2@gmail.com,
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
